70885b065bac5d647ee5ae000a63b3a15ecff9e7
Sandeep10031998 <47169568+Sandeep10031998@users.noreply.github.com>
2020-09-02 14:05:34 +0530

    Batch 2 day 2 assignment

---
+++ assignment day 2.js
@@ -0,0 +1,30 @@
+// ***** 1.Program to search for a particular character in a string *****
+
+/*let Hero = "Iron Man";
+let res=Hero.charAt(3)
+console.log(res)*/
+
+// ***** 2.Program to convert minutes to second *****
+
+/*let minutes=20;
+let res=Math.floor(minutes * 60);
+console.log(res)*/
+
+// ***** 3.Program to search for an element in a array of string *****
+
+/*let computer = [ "mouse", "keyboard", "monitor", "camera", "Hard disk"];
+let res=computer.indexOf("camera")
+console.log(res)*/
+
+// ***** 4.Program to display only element containing 'a' in them from a array *****
+
+/*let computer = [ "mouse", "keyboard", "monitor", "camera", "Hard disk"];
+let match=computer.filter(a=> a.includes('a'));
+console.log(match)*/
+
+// ***** 5.Print an array in reverse order *****
+
+let numbers = [1,2,3,4,5,6,7,8,9,10];
+let result = numbers.reverse(numbers)
+console.log(result)
+
